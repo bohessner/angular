f281cef876cb3cf56cc38589df46a320581e0f73
bohessner <bo.hessner@gmail.com>
2017-07-24 21:13:16 +0200

    Menu and routing implemented.

--- README.md
+++ README.md
@@ -3,6 +3,23 @@
 
 A simple menu and three pages are created and handled by routing
 
+## Created components:
+
+<pre>
+ng g component pages/home
+ng g component pages/about
+ng g component pages/courses
+</pre>
+
+Implemented this, but with pages above:
+https://angular.io/guide/router
+
+This also looked interesting:
+http://blog.angular-university.io/angular-2-router-nested-routes-and-nested-auxiliary-routes-build-a-menu-navigation-system
+
+## Angular CLI overview
+https://github.com/angular/angular-cli/wiki
+
 ## Git info
 
 When a new branch are created remotely then use "git checkout branchname" to get it locally, and "git branch -a" to see all branches.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,50 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+
+const routerConfig: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'courses',
+        component: CoursesComponent
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    CoursesComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      routerConfig,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
